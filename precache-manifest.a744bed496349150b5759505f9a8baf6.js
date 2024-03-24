04a649c1b627ef879dff4264ca3cedbbbdb1d0e6
AlbieryC <164671879+AlbieryC@users.noreply.github.com>
2024-03-24 00:01:15 -0400

    Updates

---
+++ precache-manifest.a744bed496349150b5759505f9a8baf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1347b5de86025456c9858f38eb60cee0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "50c41d55dc9f3a5aadb0",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "24ae7763bcdb516d7fa6",
+    "url": "/portfolio/static/css/main.8116c173.chunk.css"
+  },
+  {
+    "revision": "50c41d55dc9f3a5aadb0",
+    "url": "/portfolio/static/js/2.ed8cccaf.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.ed8cccaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ae7763bcdb516d7fa6",
+    "url": "/portfolio/static/js/main.668a931e.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
